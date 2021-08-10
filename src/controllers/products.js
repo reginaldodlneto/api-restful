e1dbc45e3f426396ee0cdb61ff41b1860be06f13
reginaldodlneto <reginaldo.dlneto@gmail.com>
2021-08-10 00:32:01 -0300

    adicionando metodo get

--- src/controllers/products.js
+++ src/controllers/products.js
@@ -1,7 +1,11 @@
 const ProductsModel = require('../models/products')
 
 async function get(req, res) {
-  const products = await ProductsModel.find()
+  const { id } = req.params
+
+  const obj = id ? { _id: id } : null
+
+  const products = await ProductsModel.find(obj)
 
   res.send(products)
 }
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -11,7 +11,7 @@ const ProductController = require('../controllers/products')
 
 */
 
-router.get('/products', ProductController.get)
+router.get('/products/:id?', ProductController.get) // Usa '?' para indicar que o parametro (:id) é opcional
 // router.post('/products', ProductController.post)
 // router.put('/products/:id', ProductController.put)
 // router.delete('/products/:id', productsontroller.delete)
